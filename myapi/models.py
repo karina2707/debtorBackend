bed8398c06fc28a958b6bdd7ec8ea29553dbf2ec
Karina Tuichieva <karina.tuichieva2707@gmail.com>
2021-02-28 18:39:50 +0300

    added id to address

--- myapi/models.py
+++ myapi/models.py
@@ -2,9 +2,9 @@
 # models.py
 from django.db import models
 class Wallet(models.Model):
-    debtorId = models.IntegerField(unique=True)
-    name = models.CharField(max_length=60)
-    debtAmount = models.IntegerField()
+    debtorId = models.IntegerField("id", primary_key=True, unique=True)
+    name = models.CharField("name", max_length=60)
+    debtAmount = models.IntegerField("Debt amount")
 
     def __str__(self):
         return self.name
